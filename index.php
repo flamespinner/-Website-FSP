2b6a41c3b9acc124b0952ed0734894a1a40a4ee7
Michael <mwilke16@moundsparkacademy.org>
2013-04-18 14:19:37 -0500

    Placeholder

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<phpinclude('header.php');?>	<div id= body>		<div id= arrow>			<img src="#" alt="this is an arrow">		</div>		</div><phpinclude('footer.php');?>
+<phpinclude('header.php');?>	<div id= body>
+		<h1> Flamespinner Productions </h1>
+			<h4> I regret to inform you that, We have moved servers. and in server moving, and a few hard rives later, I am missing all Content form my last Backup oh the site. This will be fixed shortly. </h4>
+	<h4>Thanks -Michael (Head Admin)</h4>		<!--<div id= arrow>			<img src="#" alt="this is an arrow">		</div>-->		</div><phpinclude('footer.php');?>
