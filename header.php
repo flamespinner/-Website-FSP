221cdb67ebee34edc1f79299dddbee395db0ea6c
Michael <mwilke16@moundsparkacademy.org>
2013-04-22 21:20:55 -0500

    Styles

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<!-- This site was written and developed by Michael Wilke of Flamespinner Productions -->
+<html>
+    <head>
+    	<link rel="stylesheet" href="CSS/Styles.css" />
+    	<link rel='stylesheet' media='screen and (min-width: 701px) and (max-width: 1100px)' href='CSS/tablet.css' />
+        <link rel='stylesheet' media='screen and (max-width: 700px)' href='CSS/phone.css' />
+        <title>FSP | Home</title>
+    </head>
+    
+    
+    <div id= "header">
+    	<div id="logo">
+    		<h1>Flamespinner Productions</h1>
+    	</div>
+    		<div id="nav">
+    			<p><a href="#">Nav1</a> &#8226 <a href="#">Nav2</a> &#8226 <a href="#">Nav3</a> &#8226 <a href="#">Nav4</a> &#8226 <a href="#">Nav5</a> &#8226 <a href="#games">Games</a></p>
+    		<div>
+    </div>
--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<phpinclude('header.php');?>	<div id= body>
-		<a href="indea.php"><h1> Flamespinner Productions </h1></a>
-			<h4> I regret to inform you that, We have moved servers. and in server moving, and a few hard rives later, I am missing all Content form my last Backup oh the site. This will be fixed shortly. </h4>
-	<h4>Thanks -Michael (Head Admin)</h4>		<!--<div id= arrow>			<img src="#" alt="this is an arrow">		</div>-->		</div><phpinclude('footer.php');?>
+ <head>
+ 	<link rel="stylesheet" href="CSS/Styles.css" />
+ 	<link href="/CSS/tablet.css" media="screen and (min-width: 701px) and (max-width: 1100px)" rel="stylesheet"></link>
+	<link href="/CSS/phone.css" media="screen and (max-width: 700px)" rel="stylesheet"></link>
+ </head>
+    <body>
+    
+<div class="view" id="view1">
+    <div style="position: relative; top: 30%;">
+        <div class="logo">
+            <h1>Flamespinner</h1>
+            <h2>Productions</h2>
+            <h3> Be back soon! </h3> 
+           <!-- <a href="#view1"><img src="Images/arrowred.svg" alt="enter" class="downarrow" id="arrow1" /></a> -->
+        </div>
+        
+<!--<div id="nav">
+	<p><a href="#">Nav1</a> &#8226 <a href="#">Nav2</a> &#8226 <a href="#">Nav3</a> &#8226 <a href="#">Nav4</a> &#8226 <a href="#">Nav5</a> &#8226 <a href="#games">Games</a></p>
+</div>-->
+		<div>
+    </div>
+    </body>
