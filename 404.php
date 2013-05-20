c49e202b2ffaf7e646f3139d5cd8ec3e4a36068a
Michael <mwilke16@moundsparkacademy.org>
2013-05-20 14:53:46 -0500

    added new site pieces

--- 404.php
+++
@@ -1,5 +0,0 @@
-<h1>404 Page Not Found!</h1>
-
-<p>If you think is is a mistake please <a href="mailto:mwilke16@moundsparkacademy.org">alert</a> one of the team about this problem.</p>
-<br />
-<p>If you are typing random URLs. STOP</p>
